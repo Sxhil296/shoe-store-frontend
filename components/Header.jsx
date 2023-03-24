3043e0e70c2381e42e0eeb37226738f3cbef6400
Sahil <leviack629@gmail.com>
2023-03-24 10:27:53 +0530

    mobie menu and scroll effect

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,19 +1,45 @@
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
 import Menu from "./Menu";
+import MenuMobile from "./MenuMobile";
 import Wrapper from "./Wrapper";
+
+import { IoMdHeartEmpty } from "react-icons/io";
+import { BsCart } from "react-icons/bs";
+import { BiMenuAltRight } from "react-icons/bi";
+import { VscChromeClose } from "react-icons/vsc";
 
 const Header = () => {
   const [mobileMenu, setMobileMenu] = useState(false);
   const [showCatMenu, setShowCatMenu] = useState(false);
   const [show, setShow] = useState("translation-y-0");
-  const [lastScrollY, setScrollY] = useState(0);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (window.scrollY > 200) {
+      if (window.scrollY > lastScrollY && !mobileMenu) {
+        setShow("-translate-y-[80px]");
+      } else {
+        setShow("shadow-sm");
+      }
+    } else {
+      setShow("translate-y-0");
+    }
+    setLastScrollY(window.scrollY);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", controlNavbar);
+    return () => {
+      window.removeEventListener("scroll", controlNavbar);
+    };
+  }, [lastScrollY]);
 
   return (
     <header
       className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transform duration-300 ${show}`}
     >
-      <Wrapper>
+      <Wrapper className="h-[60px] flex justify-between items-center">
         <Link href="/">
           <img
             src="/assets/logo.svg"
@@ -21,7 +47,52 @@ const Header = () => {
             className="w-[40px] md:w-[60px]"
           />
         </Link>
-        <Menu />
+        <Menu showCatMenu={showCatMenu} setShowCatMenu={setShowCatMenu} />
+
+        {mobileMenu && (
+          <MenuMobile
+            showCatMenu={showCatMenu}
+            setShowCatMenu={setShowCatMenu}
+            setMobileMenu={setMobileMenu}
+            // categories={categories}
+          />
+        )}
+
+        <div className="flex items-center gap-2 text-black">
+          {/* Heart Icon Start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <IoMdHeartEmpty className="text-[15px] md:text-[20px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              52
+            </div>
+          </div>
+          {/* Heart Icon End */}
+
+          {/* Cart Icon End */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative">
+            <BsCart className="text-[15px] md:text-[20px]" />
+            <div className="h-[14px] md:h-[18px] min-w-[14px] md:min-w-[18px] rounded-full bg-red-600 absolute top-1 left-5 md:left-7 text-white text-[10px] md:text-[12px] flex justify-center items-center px-[2px] md:px-[5px]">
+              5
+            </div>
+          </div>
+          {/* Cart Icon End */}
+
+          {/* Mobile Icon Start */}
+          <div className="w-8 md:w-12 h-8 md:h-12 rounded-full flex justify-center items-center hover:bg-black/[0.05] cursor-pointer relative -right-2">
+            {mobileMenu ? (
+              <VscChromeClose
+                className="text-[16px] md:hidden"
+                onClick={() => setMobileMenu(false)}
+              />
+            ) : (
+              <BiMenuAltRight
+                className="text-[20px] md:hidden"
+                onClick={() => setMobileMenu(true)}
+              />
+            )}
+          </div>
+          {/* Mobile Icon End */}
+        </div>
       </Wrapper>
     </header>
   );
